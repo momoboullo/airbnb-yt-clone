d3d4e308e742ed3a4480148f48b20eb0c329e07e
mohamed ali <52109014+momoboullo@users.noreply.github.com>
2021-08-08 16:39:08 +0530

    Add files via upload

---
+++ pages/search.js
@@ -0,0 +1,78 @@
+import Header from "../components/Header";
+import React from "react";
+import Footer from "../components/Footer";
+import { useRouter } from "next/router";
+import { format } from "date-fns";
+import InfoCard from "../components/InfoCard";
+
+function Search({ searchResults }) {
+  const router = useRouter();
+  const { location, startDate, endDate, noOfGuests } = router.query;
+
+  const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
+  const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
+  const range = `${formattedStartDate} - ${formattedEndDate}`;
+
+  return (
+    <div className="bg-gray-200">
+      <Header placeholder={`${location} | ${range} | ${noOfGuests}`} />
+
+      <main className="flex m-3 bg-white rounded-xl shadow-md">
+        <section className="flex-grow pt-14 px-6">
+          <p className="text-xs">
+            300+ Stays - {range} - for {noOfGuests} guests
+          </p>
+
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stays is {location}
+          </h1>
+
+          <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+            <p className="button">Cancellation Flexibility</p>
+            <p className="button">Type of Place</p>
+            <p className="button">Price</p>
+            <p className="button">Rooms and Beds</p>
+            <p className="button">More filters</p>
+          </div>
+          <div className="flex flex-col">
+            {searchResults.map(
+              ({ img, location, title, description, star, price, total }) => (
+                <InfoCard
+                  key={img}
+                  img={img}
+                  location={location}
+                  title={title}
+                  description={description}
+                  star={star}
+                  price={price}
+                  total={total}
+                />
+              )
+            )}
+          </div>
+        </section>
+      </main>
+
+      <Footer />
+      <div className=" relative text-red-600 p-2 text-center">
+        <h1 className="text-sm sm:text-lg">
+          This Clone Was Created By MOHAMED ALI BOULLO
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
+
+export async function getServerSideProps() {
+  const searchResults = await fetch("https://links.papareact.com/isz").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      searchResults,
+    },
+  };
+}
